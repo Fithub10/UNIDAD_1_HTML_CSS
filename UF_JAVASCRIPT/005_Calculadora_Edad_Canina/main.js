fa7d405d7f95fbf689ba61b59e68525aea298c6a
Fithub10 <Xavi.domingo95@gmail.com>
2025-02-11 20:33:09 +0100

    Ejercicio Calculadora

---
+++ UF_JAVASCRIPT/005_Calculadora_Edad_Canina/main.js
@@ -0,0 +1,18 @@
+/*Funcion que pide la edad del perro*/
+
+let edadperro = parseInt(prompt("Introduce la edad actual de tu perro"));
+document.write(`<br> Edad Animal del perro: ${num1}`);
+
+
+
+/*Funcion que calcula la edad del perro *7 y lo muestra en document.write */
+
+let edadhumano = parseInt(prompt("La edad de su perro se multiplicara *7 para saber su conversion a edad humana "));
+document.write(`<br> \n Segundo número introducido: ${7}`);
+
+
+
+/*Funcion que entrega la edad humana del perro despues lo muestra  */
+
+let calculo = edadperro * edadhumano;
+document.write(`<br> \n la edad real de el perro es : ${calculo}`);
